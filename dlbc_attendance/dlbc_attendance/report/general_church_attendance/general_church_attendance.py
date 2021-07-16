213b2da788c2d0b09b78edd57946a0de6b68eff8
ugo <ugo@localhost.localdomain>
2021-07-16 22:51:53 +0100

    added filters

--- dlbc_attendance/dlbc_attendance/report/general_church_attendance/general_church_attendance.py
+++ dlbc_attendance/dlbc_attendance/report/general_church_attendance/general_church_attendance.py
@@ -9,10 +9,19 @@ def execute(filters=None):
 
 
 def get_data(filters):
-	_from, to = filters.get('from'), filters.get('to')
+	_from, to = filters.get('from'), filters.get('to')#date range
+	conditions = " AND 1=1 "
+	if(filters.get('district_church_name')):conditions += f" AND district_church_name='{filters.get('district_church_name')}' "
+	if(filters.get('group_of_districts_name')):conditions += f" AND group_of_districts_name='{filters.get('group_of_districts_name')}' "
+	if(filters.get('old_group_or_lga_name')):conditions += f" AND district_church_name='{filters.get('old_group_or_lga_name')}' "
+	if(filters.get('region')):conditions += f" AND region='{filters.get('region')}' "
+	if(filters.get('state')):conditions += f" AND state='{filters.get('state')}' "
+	if(filters.get('country_name')):conditions += f" AND state='{filters.get('country_name')}' "
+	if(filters.get('dlbc_weekly_activities')):conditions += f" AND state='{filters.get('dlbc_weekly_activities')}' "
+
 	data = frappe.db.sql(f"""SELECT district_church_name,group_of_districts_name,
-			old_group_or_lga_name,region,state,country_name FROM `tabDLBC Attendance Register` WHERE
-			(creation BETWEEN {_from} AND {to});""")
+			old_group_or_lga_name,region,state,country_name, dlbc_weekly_activities FROM
+			`tabDLBC Attendance Register` WHERE (creation BETWEEN '{_from}' AND '{to}');""")
 	return data
 
 def get_columns():
